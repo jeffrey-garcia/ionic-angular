4d01f298a7f3fe83b69551a6aa8c4325e209ba7a
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2018-07-24 15:45:08 +0800

    add sample to subscribe to scroll events and dynamically control header bar style

--- src/app/leads/leads-create/leads-create.component.ts
+++ src/app/leads/leads-create/leads-create.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'app-leads-create',
   templateUrl: './leads-create.component.html',
-  styleUrls: ['./leads-create.component.css']
+  styleUrls: ['./leads-create.component.css','./leads-create.component.scss']
 })
 export class LeadsCreateComponent implements OnInit {
+  @ViewChild(Content) content:Content;
+
+  private enable:boolean = false;
 
   constructor(
     private navCtrl:NavController
@@ -16,4 +19,20 @@ export class LeadsCreateComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.content.ionScroll.subscribe(($event) => {
+      console.log($event.scrollTop);
+      if ($event.scrollTop != null) {
+        if ($event.scrollTop>=10) {
+          this.enable = true;
+        } else {
+          this.enable = false;
+        }
+      }
+    });
+  }
+
+  public shouldEnableBlackTitle(): boolean {
+    return this.enable;
+  }
 }
