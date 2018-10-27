07912513e550dae3e1360f6859d844e54e0e95d5
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2018-10-27 10:54:02 +0800

    add startup splash page before Angular app is bootstrapped into browser

--- src/scripts/coreHybridApp.js
+++ src/scripts/coreHybridApp.js
@@ -4,13 +4,13 @@
         let onDeviceReady = function(){
             console.log("cordova app: device ready");
             window.ngAppComponent.cordovaReady();
-            //document.addEventListener('backbutton', this.onBackKeyDown, false);
+            //document.addEventListener('backbutton', onBackKeyDown, false);
         };
 
         let onBackKeyDown = function(){
             console.log("hard-back button clicked");
             //window.ngAppComponent.onBackPressed();
-        }
+        };
 
         console.log("initializing cordova app...");
         document.addEventListener('deviceready', onDeviceReady.bind(this), false);
